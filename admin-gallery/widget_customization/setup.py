57e58fa6f027bdb6ff8f0e36fa7489d94f9326a2
vladiibine <vlad.ardelean@3pillarglobal.com>
2015-10-08 17:29:50 +0300

    packaging stuff

--- admin-gallery/widget_customization/setup.py
+++ admin-gallery/widget_customization/setup.py
@@ -7,6 +7,8 @@ setup(
     url='https://github.com/vladiibine/',
     license='MIT',
     author='Vlad-George Ardelean',
+    include_package_data=True,
+    package_data={'model_select_customized': ['db.sqlite3']},
     author_email='',
     description='Example project showing various admin customization options',
     install_requires=['Django>=1.8,<1.9'],
